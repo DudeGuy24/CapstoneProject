d8f433e10fda0e29e7e3b305ff89af15c58425f4
Madhia <24397644@sun.ac.za>
2024-05-26 15:55:27 +0200

    Cleaned code

--- project/myListener.java
+++ project/myListener.java
@@ -26,11 +26,6 @@ public class myListener extends antlrParserBaseListener{
     
     @Override public void enterParserRuleSpec(antlrParser.ParserRuleSpecContext ctx) { 
         ruleRef = ctx.RULE_REF().getText();
-        //System.out.println(ruleRef);
-
-        // for (int i = 0; i < ctx.ruleBlock().getChildCount(); i++) {
-        //     String childText = ctx.ruleBlock().getChild(i).getText();
-        // }
     }
 
     @Override public void exitParserRuleSpec(antlrParser.ParserRuleSpecContext ctx) { 
@@ -38,29 +33,13 @@ public class myListener extends antlrParserBaseListener{
         ruleBlockChildrenTexts = new ArrayList<>();
     }
 
-    @Override public void enterRuleBlock(antlrParser.RuleBlockContext ctx) { 
-    }
-
-    @Override public void enterRuleAltList(antlrParser.RuleAltListContext ctx) { 
-    }
-
-    @Override public void enterLabeledAlt(antlrParser.LabeledAltContext ctx) { 
-    }
-
     @Override public void enterAlternative(antlrParser.AlternativeContext ctx) { 
         /*  : elementOptions? element+ | // explicitly allow empty alts */
-        // if (ctx.elementOptions() != null) {
-        //     String childText = ctx.elementOptions().getText();
-        //     ruleBlockChildrenTexts.add(childText);
-        // }
         if (ctx.element() != null) {
             for (antlrParser.ElementContext el : ctx.element()) {
                 String childText = el.getText();
-                //System.out.println(childText);
                 ruleBlockChildrenTexts.add(childText);
             }
         }
-        //System.out.println("Finished saving elements into ruleBlockChildren");
-        //System.out.println("ruleMap " + ruleMap);
     }
 }
