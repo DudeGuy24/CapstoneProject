7e24cac308a3522c0456ec40ea02d76e79114aa3
Bosma, BP, Mr [23537795@sun.ac.za] <23537795@sun.ac.za>
2024-05-29 14:44:35 +0200

    L and R recursion separate detection

--- project/myListener.java
+++ project/myListener.java
@@ -4,20 +4,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class myListener extends antlrParserBaseListener{
-    
+public class myListener extends antlrParserBaseListener {
+
     private File file = null;
     private Map<String, List<List<String>>> ruleMap = new HashMap<>();
     private String ruleRef;
     private List<List<String>> ruleBlockChildrenTexts = new ArrayList<>();
     private List<String> alternativeList = new ArrayList<>();
-    
+
     /**
      * Sets the file currently being processed.
      *
      * @param file The file to set.
      */
-    public void setFile(File file){
+    public void setFile(File file) {
         this.file = file;
     }
 
@@ -25,38 +25,48 @@ public class myListener extends antlrParserBaseListener{
         for (Map.Entry<String, List<List<String>>> entry : ruleMap.entrySet()) {
             String key = entry.getKey();
             List<List<String>> value = entry.getValue();
-            
+
             System.out.println("Key: " + key);
-            boolean keyFound = false;
+            boolean leftRecursionFound = false;
+            boolean rightRecursionFound = false;
 
             for (List<String> innerList : value) {
                 System.out.print("  [");
-                for (String s : innerList) {
+                for (int i = 0; i < innerList.size(); i++) {
+                    String s = innerList.get(i);
                     System.out.print(s + " ");
-                    if (s.equals(key)) {
-                        keyFound = true;
+                    if (i == 0 && s.equals(key)) {
+                        leftRecursionFound = true;
+                    }
+                    if (i == innerList.size() - 1 && s.equals(key)) {
+                        rightRecursionFound = true;
                     }
                 }
                 System.out.println("]");
             }
-            if (keyFound) {
-                System.out.println("This rule contains LR recursion");
+            if (leftRecursionFound) {
+                System.out.println("This rule contains left recursion");
+            }
+            if (rightRecursionFound) {
+                System.out.println("This rule contains right recursion");
             }
             System.out.println("");
         }
     }
-    
-    @Override public void enterParserRuleSpec(antlrParser.ParserRuleSpecContext ctx) { 
+
+    @Override
+    public void enterParserRuleSpec(antlrParser.ParserRuleSpecContext ctx) {
         ruleRef = ctx.RULE_REF().getText();
     }
 
-    @Override public void exitParserRuleSpec(antlrParser.ParserRuleSpecContext ctx) { 
+    @Override
+    public void exitParserRuleSpec(antlrParser.ParserRuleSpecContext ctx) {
         ruleMap.put(ruleRef, ruleBlockChildrenTexts);
         ruleBlockChildrenTexts = new ArrayList<>();
     }
 
-
-    @Override public void enterAlternative(antlrParser.AlternativeContext ctx) { 
+    @Override
+    public void enterAlternative(antlrParser.AlternativeContext ctx) {
         /*  : elementOptions? element+ | // explicitly allow empty alts */
         if (ctx.element() != null) {
             for (antlrParser.ElementContext el : ctx.element()) {
@@ -66,7 +76,8 @@ public class myListener extends antlrParserBaseListener{
         }
     }
 
-    @Override public void exitAlternative(antlrParser.AlternativeContext ctx) {
+    @Override
+    public void exitAlternative(antlrParser.AlternativeContext ctx) {
         ruleBlockChildrenTexts.add(alternativeList);
         alternativeList = new ArrayList<>();
     }
